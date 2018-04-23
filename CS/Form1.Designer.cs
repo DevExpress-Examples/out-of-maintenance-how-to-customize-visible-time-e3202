d5cc1093dce8bd58bb5808ee3482ad4a8b604824
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SchedulerNonStandardTimeInterval
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.rangeTrackBarControl1 = new DevExpress.XtraEditors.RangeTrackBarControl();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rangeTrackBarControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rangeTrackBarControl1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.schedulerControl1.Location = new System.Drawing.Point(84, 15);
+            this.schedulerControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(751, 418);
+            this.schedulerControl1.Start = new System.DateTime(2008, 9, 4, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            this.schedulerControl1.Views.MonthView.Enabled = false;
+            this.schedulerControl1.Views.TimelineView.Enabled = false;
+            this.schedulerControl1.Views.WeekView.Enabled = false;
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            // 
+            // rangeTrackBarControl1
+            // 
+            this.rangeTrackBarControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.rangeTrackBarControl1.EditValue = new DevExpress.XtraEditors.Repository.TrackBarRange(18, 42);
+            this.rangeTrackBarControl1.Location = new System.Drawing.Point(16, 15);
+            this.rangeTrackBarControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.rangeTrackBarControl1.Name = "rangeTrackBarControl1";
+            this.rangeTrackBarControl1.Properties.Maximum = 48;
+            this.rangeTrackBarControl1.Properties.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.rangeTrackBarControl1.Properties.ShowValueToolTip = true;
+            this.rangeTrackBarControl1.Size = new System.Drawing.Size(56, 418);
+            this.rangeTrackBarControl1.TabIndex = 0;
+            this.rangeTrackBarControl1.Value = new DevExpress.XtraEditors.Repository.TrackBarRange(18, 42);
+            this.rangeTrackBarControl1.EditValueChanged += new System.EventHandler(this.rangeTrackBarControl1_EditValueChanged);
+            this.rangeTrackBarControl1.BeforeShowValueToolTip += new DevExpress.XtraEditors.TrackBarValueToolTipEventHandler(this.rangeTrackBarControl1_BeforeShowValueToolTip);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 447);
+            this.Controls.Add(this.rangeTrackBarControl1);
+            this.Controls.Add(this.schedulerControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rangeTrackBarControl1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rangeTrackBarControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.XtraEditors.RangeTrackBarControl rangeTrackBarControl1;
+    }
+}
+
---
+++ VB/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+
+Namespace SchedulerNonStandardTimeInterval
+	Partial Public Class Form1
+		Inherits Form
+		Public ReadOnly Property StartTimeOffset() As TimeSpan
+			Get
+				Return TimeSpan.FromHours(rangeTrackBarControl1.Properties.Maximum - rangeTrackBarControl1.Value.Maximum)
+			End Get
+		End Property
+		Public ReadOnly Property EndTimeOffset() As TimeSpan
+			Get
+				Return TimeSpan.FromHours(rangeTrackBarControl1.Properties.Maximum - rangeTrackBarControl1.Value.Minimum)
+			End Get
+		End Property
+
+		Public Sub New()
+			InitializeComponent()
+			UpdateVisibleTime()
+		End Sub
+
+		Private Sub rangeTrackBarControl1_EditValueChanged(ByVal sender As Object, ByVal e As EventArgs) Handles rangeTrackBarControl1.EditValueChanged
+			UpdateVisibleTime()
+		End Sub
+
+		Private Sub UpdateVisibleTime()
+			schedulerControl1.DayView.VisibleTime = New DevExpress.XtraScheduler.TimeOfDayInterval(StartTimeOffset, EndTimeOffset)
+			schedulerControl1.WorkWeekView.VisibleTime = New DevExpress.XtraScheduler.TimeOfDayInterval(StartTimeOffset, EndTimeOffset)
+		End Sub
+
+		Private Sub rangeTrackBarControl1_BeforeShowValueToolTip(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.TrackBarValueToolTipEventArgs) Handles rangeTrackBarControl1.BeforeShowValueToolTip
+			e.ShowArgs.ToolTip = String.Format("Interval = [{0}; {1}]", StartTimeOffset, EndTimeOffset)
+		End Sub
+	End Class
+End Namespace
